1f7c6afef2fd7d8327616a08b9845b18e1d5931b
Miłosz <43027400+miloszczapla@users.noreply.github.com>
2022-05-22 17:58:10 +0200

    Merging data frames (#5)
    merged data to 2 data frames
    still more work is needed to it

--- PrzygotowanieRamkiDanych.R
+++ PrzygotowanieRamkiDanych.R
@@ -4,17 +4,17 @@ library(countrycode)
 
 
 #kolumny: okresy czas
-zarobki_kob_euro <- read_excel('dane/zarobki_kob_euro.xlsx')
-zarobki_mez_euro <- read_excel('dane/zarobki_mez_euro.xlsx')
 cena_gazu <- read_excel('dane/cena_gazu.xlsx')
 cena_pradu <- read_excel('dane/cena_pradu.xlsx')
 wynajem <- read_excel('dane/wynajem.xlsx')
 wartosc_podatku <- read_excel('dane/wartosc_podatku.xlsx')
-Parytety_sily_nabywczej <- read_excel('dane/Parytety_sily_nabywczej.xlsx')
 inflacja <- read_csv('dane/inflacja.csv')
+zarobki_kob_euro <- read_excel('dane/zarobki_kob_euro.xlsx')
+zarobki_mez_euro <- read_excel('dane/zarobki_mez_euro.xlsx')
+Parytety_sily_nabywczej <- read_excel('dane/Parytety_sily_nabywczej.xlsx')
 bilans_zuzycia_wody <- read_excel('dane/bilans_zuzycia_wody.xlsx')
 
-#kolumny: sex, kraj, okres czasu, wartość
+#kolumny: płeć, kraj, okres czasu, wartość
 konsumpcja_owoce_warzywa <- read_csv('dane/konsumpcja_owoce_warzywa.csv')
 przewidywana_dlugosc_zycia <- read_csv('dane/przewidywana_dlugosc_zycia.csv')
 edukajca_po_populacji <- read_csv('dane/edukajca_po_populacji.csv')
@@ -52,18 +52,66 @@ bilans_zuzycia_wody <- cbind(bilans_zuzycia_wody2_1, bilans_zuzycia_wody2_2)
 
 
 #prze organizowanie ramek danych
-cena_gazu <- pivot_longer(cena_gazu,cols = !TIME, names_to = "okres czasu", values_to = "cena gaza")
-
-cena_pradu <- pivot_longer(cena_pradu,cols = !TIME, names_to = "okres czasu", values_to = "cena pradu")
-wynajem <- pivot_longer(wynajem,cols = !"GEO/TIME", names_to = "okres czasu", values_to = "cena wynajmu za miesiac")
-wartosc_podatku <- pivot_longer(wartosc_podatku,cols = !TIME, names_to = "okres czasu", values_to = "podatek jako % dochodu")
-inflacja <- pivot_longer(inflacja,cols = !geo, names_to = "okres czasu", values_to = "inflacja rok do roku")
-zarobki_kob_euro <- pivot_longer(zarobki_kob_euro[-1,],cols = !TIME, names_to = "okres czasu", values_to = "średnie roczne zarobki kobiet")
-zarobki_mez_euro <- pivot_longer(zarobki_mez_euro[-1,],cols = !TIME, names_to = "okres czasu", values_to = "średnie roczne zarobki mężczyzn")
-Parytety_sily_nabywczej <- pivot_longer(Parytety_sily_nabywczej[-1,],cols = !TIME, names_to = "okres czasu", values_to = "średnie roczne zarobki mężczyzn")
-bilans_zuzycia_wody <- pivot_longer(bilans_zuzycia_wody,cols = !TIME, names_to = "okres czasu", values_to = "żuzecie wody w mil. m^3")
-
+cena_gazu <- pivot_longer(cena_gazu,cols = !TIME, names_to = "okres_czasu", values_to = "cena_gazu")
+cena_pradu <- pivot_longer(cena_pradu,cols = !TIME, names_to = "okres_czasu", values_to = "cena_pradu")
+wynajem <- pivot_longer(wynajem,cols = !"GEO/TIME", names_to = "okres_czasu", values_to = "cena_wynajmu_a_miesiac")
+wartosc_podatku <- pivot_longer(wartosc_podatku,cols = !TIME, names_to = "okres_czasu", values_to = "podatek_jako_%_dochodu")
+inflacja <- pivot_longer(inflacja,cols = !geo, names_to = "okres_czasu", values_to = "inflacja_rok_do_roku")
+zarobki_kob_euro <- pivot_longer(zarobki_kob_euro[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "średnie_roczne_zarobki_kobiet")
+zarobki_mez_euro <- pivot_longer(zarobki_mez_euro[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "średnie_roczne_zarobki_mężczyzn")
+Parytety_sily_nabywczej <- pivot_longer(Parytety_sily_nabywczej[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "średnie_roczne_zarobki_mężczyzn")
+bilans_zuzycia_wody <- pivot_longer(bilans_zuzycia_wody,cols = !TIME, names_to = "okres_czasu", values_to = "żuzecie_wody_w_mil._m^3")
 
 
+#zamiana nazw nagłówków ramek danych
+names(cena_gazu )[1] <- 'zakres_geograficzny'
+names(cena_pradu )[1] <- 'zakres_geograficzny'
+names(wynajem )[1] <- 'zakres_geograficzny'
+names(wartosc_podatku )[1] <- 'zakres_geograficzny'
+names(inflacja )[1] <- 'zakres_geograficzny'
+names(zarobki_kob_euro )[1] <- 'zakres_geograficzny'
+names(zarobki_mez_euro )[1] <- 'zakres_geograficzny'
+names(Parytety_sily_nabywczej )[1] <- 'zakres_geograficzny'
+names(bilans_zuzycia_wody )[1] <- 'zakres_geograficzny'
+names(przewidywana_dlugosc_zycia )[length(przewidywana_dlugosc_zycia)]<- 'przewidywana_dlugosc_zycia'
+names(edukajca_po_populacji )[length(edukajca_po_populacji)]<- 'edukajca'
+names(konsumpcja_owoce_warzywa )[length(konsumpcja_owoce_warzywa)]<- 'konsumpcja_owoce_warzywa'
+names(konsumpcja_owoce_warzywa )[5]<- 'geography'
+names(konsumpcja_owoce_warzywa )[6]<- 'date'
+
+
+#łączenie 1 ramki danych
+
+#redukcja precyzji do danych rocznych
+
+
+#Potrzebna lepsza nazwa!!!
+dt_1 <- merge(cena_gazu,cena_pradu,by = c('zakres_geograficzny','okres_czasu'), all= TRUE)
+dt_1$okres_czasu <- str_remove(dt_1$okres_czasu,"-S[12]")
+dt_1 <- dt_1 %>%
+  group_by(okres_czasu,zakres_geograficzny) %>%
+  summarise(cena_gazu = mean(cena_gazu, na.rm = FALSE),
+            cena_pradu = mean(cena_pradu, na.rm = FALSE))
+ 
+dt_1 <- dt_1 %>%
+  merge(  wynajem, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+merge(wartosc_podatku, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+merge(inflacja, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+merge(zarobki_kob_euro, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+merge(zarobki_mez_euro, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+merge(Parytety_sily_nabywczej, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+merge(bilans_zuzycia_wody, by = c('zakres_geograficzny','okres_czasu'), all= TRUE)
+
+#łączenie 2 ramki danych
+konsumpcja_owoce_warzywa<-konsumpcja_owoce_warzywa %>%
+  group_by(unit,n_portion,sex,age,country,time) %>%
+  summarise(konsumpcja_owoce_warzywa = mean(konsumpcja_owoce_warzywa, na.rm = FALSE))
+przewidywana_dlugosc_zycia[1,]
+edukajca_po_populacji[1,]
+konsumpcja_owoce_warzywa[1,]
+
+#Potrzebna lepsza nazwa!!!
+dt_2 <- merge(konsumpcja_owoce_warzywa[,-which(names(konsumpcja_owoce_warzywa) %in% c("unit","n_portion",'age'))],edukajca_po_populacji[,-which(names(edukajca_po_populacji) %in% c("unit","isced11",'age'))],by = c('geography','date','sex')) %>%
+  merge(przewidywana_dlugosc_zycia[,-which(names(przewidywana_dlugosc_zycia) %in% c("unit","statinfo"))], by = c('geography','date','sex'))
 
 
