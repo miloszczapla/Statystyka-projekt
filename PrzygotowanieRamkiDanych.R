2952f7bfd22b0afc1051983f09f76bfebd00dee0
miloszczapla <miloszczapla@gmail.com>
2022-05-22 13:37:19 +0200

    Reorganised data for inflation, earnings, tax, cost of gas, cost of energy

--- PrzygotowanieRamkiDanych.R
+++ PrzygotowanieRamkiDanych.R
@@ -1,27 +1,33 @@
 library(tidyverse)
 library(readxl)
+library(countrycode)
 
-zarobki_kob_euro <- read_excel('dane/zarobki_kob_euro.xlsx'  )
+
+#kolumny: okresy czas
+zarobki_kob_euro <- read_excel('dane/zarobki_kob_euro.xlsx')
 zarobki_mez_euro <- read_excel('dane/zarobki_mez_euro.xlsx')
 cena_gazu <- read_excel('dane/cena_gazu.xlsx')
 cena_pradu <- read_excel('dane/cena_pradu.xlsx')
 wynajem <- read_excel('dane/wynajem.xlsx')
 wartosc_podatku <- read_excel('dane/wartosc_podatku.xlsx')
-konsumpcja_owoce_warzywa <- read_csv('dane/konsumpcja_owoce_warzywa.csv')
+Parytety_sily_nabywczej <- read_excel('dane/Parytety_sily_nabywczej.xlsx')
+inflacja <- read_csv('dane/inflacja.csv')
+bilans_zuzycia_wody <- read_excel('dane/bilans_zuzycia_wody.xlsx')
 
+#kolumny: sex, kraj, okres czasu, wartość
+konsumpcja_owoce_warzywa <- read_csv('dane/konsumpcja_owoce_warzywa.csv')
 przewidywana_dlugosc_zycia <- read_csv('dane/przewidywana_dlugosc_zycia.csv')
 edukajca_po_populacji <- read_csv('dane/edukajca_po_populacji.csv')
-Parytety_sily_nabywczej <- read_excel('dane/Parytety_sily_nabywczej.xlsx')
 
-inflacja <- read_csv('dane/inflacja.csv')
-fundusze_europejskie <- read_csv('dane/fundusze_europejskie.csv')
 kurs_euro_dolar <- read_csv('dane/kurs_euro_dolar.csv')
-eksport_odpadow <- read_excel('dane/eksport_odpadow.xlsx')
-bilans_zuzycia_wody <- read_excel('dane/bilans_zuzycia_wody.xlsx')
+
 bilans_energetyczny <- read_csv('dane/bilans_energetyczny.csv')
+eksport_odpadow <- read_excel('dane/eksport_odpadow.xlsx')
+
+fundusze_europejskie <- read_csv('dane/fundusze_europejskie.csv')
 dochod <- read_csv('dane/dochod.csv')
 
-#CENA GAZU
+#CENA GAZU 
 #doprowadzenie, by cala tabela byla widoczna
 cena_gazu_1 <- cena_gazu[-1,1]
 cena_gazu_2 <- sapply(cena_gazu[-1,-1],as.numeric)
@@ -67,3 +73,22 @@ bilans_zuzycia_wody
 bilans_energetyczny
 #dochod ankieta
 dochod
+
+
+
+#prze organizowanie ramek danych
+cena_gazu <- pivot_longer(cena_gazu,cols = !TIME, names_to = "okres czasu", values_to = "cena gaza")
+
+cena_pradu <- pivot_longer(cena_pradu,cols = !TIME, names_to = "okres czasu", values_to = "cena pradu")
+wynajem <- pivot_longer(wynajem,cols = !"GEO/TIME", names_to = "okres czasu", values_to = "cena wynajmu za miesiac")
+wartosc_podatku <- pivot_longer(wartosc_podatku,cols = !TIME, names_to = "okres czasu", values_to = "podatek jako % dochodu")
+inflacja <- pivot_longer(inflacja,cols = !geo, names_to = "okres czasu", values_to = "inflacja rok do roku")
+zarobki_kob_euro <- pivot_longer(zarobki_kob_euro[-1,],cols = !TIME, names_to = "okres czasu", values_to = "średnie roczne zarobki kobiet")
+zarobki_mez_euro <- pivot_longer(zarobki_mez_euro[-1,],cols = !TIME, names_to = "okres czasu", values_to = "średnie roczne zarobki mężczyzn")
+Parytety_sily_nabywczej <- pivot_longer(Parytety_sily_nabywczej[-1,],cols = !TIME, names_to = "okres czasu", values_to = "średnie roczne zarobki mężczyzn")
+bilans_zuzycia_wody <- pivot_longer(bilans_zuzycia_wody,cols = !TIME, names_to = "okres czasu", values_to = "żuzecie wody w mil. m^3")
+
+
+
+
+
