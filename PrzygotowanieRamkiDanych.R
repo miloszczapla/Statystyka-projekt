b4eaa8511217893dd2107ddc5cad73d6abbe37c7
miloszczapla <miloszczapla@gmail.com>
2022-05-22 13:44:45 +0200

    picking data which we will not use, at least not now, deleting not inportant lines of code and coments

--- PrzygotowanieRamkiDanych.R
+++ PrzygotowanieRamkiDanych.R
@@ -24,55 +24,30 @@ kurs_euro_dolar <- read_csv('dane/kurs_euro_dolar.csv')
 bilans_energetyczny <- read_csv('dane/bilans_energetyczny.csv')
 eksport_odpadow <- read_excel('dane/eksport_odpadow.xlsx')
 
-fundusze_europejskie <- read_csv('dane/fundusze_europejskie.csv')
-dochod <- read_csv('dane/dochod.csv')
+#najpewniej nie będziemy z nich korzystać
+#fundusze_europejskie <- read_csv('dane/fundusze_europejskie.csv')
+#dochod <- read_csv('dane/dochod.csv')
 
-#CENA GAZU 
-#doprowadzenie, by cala tabela byla widoczna
+#zmiana typów danych tam gdzie jest to potrzebne
 cena_gazu_1 <- cena_gazu[-1,1]
 cena_gazu_2 <- sapply(cena_gazu[-1,-1],as.numeric)
 cena_gazu <- cbind(cena_gazu_1, cena_gazu_2)
-cena_gazu
-#CENA PRADU
+
 cena_pradu_1 <- cena_pradu[-1,1]
 cena_pradu_2 <- sapply(cena_pradu[-1,-1],as.numeric)
 cena_pradu <- cbind(cena_pradu_1, cena_pradu_2)
-cena_pradu
-#wynajem
+
 wynajem_1 <- wynajem[-1,1]
 wynajem_2 <- sapply(wynajem[-1,-1],as.numeric)
 wynajem <- cbind(wynajem_1, wynajem_2)
-wynajem
-#wartosc podatku
+
 wartosc_podatku_1 <- wartosc_podatku[-1,1]
 wartosc_podatku2_2 <- sapply(wartosc_podatku[-1,-1],as.numeric)
 wartosc_podatku <- cbind(wartosc_podatku_1, wartosc_podatku2_2)
-wartosc_podatku
-#przewidywana dlugosc zycia
-przewidywana_dlugosc_zycia
-#populacja/poziom wykształcenia
-edukajca_po_populacji
-#parytet sily nabywczej
-Parytety_sily_nabywczej
-#inflacja
-inflacja
-#fundusze europejskie
-fundusze_europejskie
-#euro i dolar
-kurs_euro_dolar
-#eksport odpadow
-eksport_odpadow
-#dzienna konsumpcja owocow i warzyw
-konsumpcja_owoce_warzywa
-#bilans zuzycia wody w mln^3
+
 bilans_zuzycia_wody2_1 <- bilans_zuzycia_wody[-1,1]
 bilans_zuzycia_wody2_2 <- sapply(bilans_zuzycia_wody[-1,-1],as.numeric)
 bilans_zuzycia_wody <- cbind(bilans_zuzycia_wody2_1, bilans_zuzycia_wody2_2)
-bilans_zuzycia_wody
-#bilans energetyczny
-bilans_energetyczny
-#dochod ankieta
-dochod
 
 
 
