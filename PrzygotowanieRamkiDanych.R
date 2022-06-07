37a3d106feb3e2224c68520b5a61487243a70acc
miloszczapla <miloszczapla@gmail.com>
2022-06-07 19:25:02 +0200

    Dodano wykresy o zarobkach i cenach po roku

--- PrzygotowanieRamkiDanych.R
+++ PrzygotowanieRamkiDanych.R
@@ -8,12 +8,13 @@ library(ggplot2)
 #kolumny: okresy czas
 cena_gazu <- read_excel('dane/cena_gazu.xlsx')
 cena_pradu <- read_excel('dane/cena_pradu.xlsx')
-zmiana_cen_domow <- read_excel('dane/zmiana_cen_domow.xlsx')
+ceny_nowych_mieszkan <- read_excel('dane/ceny_nowych_mieszkan.xlsx')
+ceny_uzywanych_mieszkan <- read_excel('dane/ceny_uzywanych_mieszkan.xlsx')
 wartosc_podatku <- read_excel('dane/wartosc_podatku.xlsx')
 inflacja <- read_csv('dane/inflacja.csv')
 zarobki_kob_euro <- read_excel('dane/zarobki_kob_euro.xlsx')
 zarobki_mez_euro <- read_excel('dane/zarobki_mez_euro.xlsx')
-Parytety_sily_nabywczej <- read_excel('dane/Parytety_sily_nabywczej.xlsx')
+parytety_sily_nabywczej <- read_excel('dane/parytety_sily_nabywczej.xlsx')
 bilans_zuzycia_wody <- read_excel('dane/bilans_zuzycia_wody.xlsx')
 
 #kolumny: płeć, kraj, okres czasu, wartość
@@ -35,6 +36,7 @@ cena_gazu_1 <- cena_gazu[-1,1]
 cena_gazu_2 <- sapply(cena_gazu[-1,-1],as.numeric)
 cena_gazu <- cbind(cena_gazu_1, cena_gazu_2)
 
+
 cena_pradu_1 <- cena_pradu[-1,1]
 cena_pradu_2 <- sapply(cena_pradu[-1,-1],as.numeric)
 cena_pradu <- cbind(cena_pradu_1, cena_pradu_2)
@@ -47,9 +49,13 @@ zarobki_mez_euro_1 <- zarobki_mez_euro[-1,1]
 zarobki_mez_euro_2 <- sapply(zarobki_mez_euro[-1,-1],as.numeric)
 zarobki_mez_euro <- cbind(zarobki_mez_euro_1, zarobki_mez_euro_2)
 
-wynajem_1 <- wynajem[-1,1]
-wynajem_2 <- sapply(wynajem[-1,-1],as.numeric)
-wynajem <- cbind(wynajem_1, wynajem_2)
+ceny_nowych_mieszkan_1 <- ceny_nowych_mieszkan[-1,1]
+ceny_nowych_mieszkan_2 <- sapply(ceny_nowych_mieszkan[-1,-1],as.numeric)
+ceny_nowych_mieszkan <- cbind(ceny_nowych_mieszkan_1, ceny_nowych_mieszkan_2)
+
+ceny_uzywanych_mieszkan_1 <- ceny_uzywanych_mieszkan[-1,1]
+ceny_uzywanych_mieszkan_2 <- sapply(ceny_uzywanych_mieszkan[-1,-1],as.numeric)
+ceny_uzywanych_mieszkan <- cbind(ceny_uzywanych_mieszkan_1, ceny_uzywanych_mieszkan_2)
 
 wartosc_podatku_1 <- wartosc_podatku[-1,1]
 wartosc_podatku2_2 <- sapply(wartosc_podatku[-1,-1],as.numeric)
@@ -64,25 +70,28 @@ bilans_zuzycia_wody <- cbind(bilans_zuzycia_wody2_1, bilans_zuzycia_wody2_2)
 #prze organizowanie ramek danych
 cena_gazu <- pivot_longer(cena_gazu,cols = !TIME, names_to = "okres_czasu", values_to = "cena_gazu")
 cena_pradu <- pivot_longer(cena_pradu,cols = !TIME, names_to = "okres_czasu", values_to = "cena_pradu")
-wynajem <- pivot_longer(wynajem,cols = !"GEO/TIME", names_to = "okres_czasu", values_to = "cena_wynajmu_za_miesiac")
 wartosc_podatku <- pivot_longer(wartosc_podatku,cols = !TIME, names_to = "okres_czasu", values_to = "podatek_jako_%_dochodu")
 inflacja <- pivot_longer(inflacja,cols = !geo, names_to = "okres_czasu", values_to = "inflacja_rok_do_roku")
 zarobki_kob_euro <- pivot_longer(zarobki_kob_euro[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "średnie_roczne_zarobki_kobiet")
 zarobki_mez_euro <- pivot_longer(zarobki_mez_euro[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "średnie_roczne_zarobki_mężczyzn")
-Parytety_sily_nabywczej <- pivot_longer(Parytety_sily_nabywczej[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "parytety_sily_nabywczej")
+parytety_sily_nabywczej <- pivot_longer(parytety_sily_nabywczej[-1,],cols = !TIME, names_to = "okres_czasu", values_to = "parytety_sily_nabywczej")
 bilans_zuzycia_wody <- pivot_longer(bilans_zuzycia_wody,cols = !TIME, names_to = "okres_czasu", values_to = "żuzecie_wody_w_mil._m^3")
+ceny_nowych_mieszkan <- pivot_longer(ceny_nowych_mieszkan,cols = !TIME, names_to = "okres_czasu", values_to = "ceny_nowych_mieszkan")
+ceny_uzywanych_mieszkan <- pivot_longer(ceny_uzywanych_mieszkan,cols = !TIME, names_to = "okres_czasu", values_to = "ceny_uzywanych_mieszkan")
+
 
 
 #zamiana nazw nagłówków ramek danych
 names(cena_gazu )[1] <- 'zakres_geograficzny'
 names(cena_pradu )[1] <- 'zakres_geograficzny'
-names(wynajem )[1] <- 'zakres_geograficzny'
 names(wartosc_podatku )[1] <- 'zakres_geograficzny'
 names(inflacja )[1] <- 'zakres_geograficzny'
 names(zarobki_kob_euro )[1] <- 'zakres_geograficzny'
 names(zarobki_mez_euro )[1] <- 'zakres_geograficzny'
-names(Parytety_sily_nabywczej )[1] <- 'zakres_geograficzny'
+names(parytety_sily_nabywczej )[1] <- 'zakres_geograficzny'
 names(bilans_zuzycia_wody )[1] <- 'zakres_geograficzny'
+names(ceny_nowych_mieszkan )[1] <- 'zakres_geograficzny'
+names(ceny_uzywanych_mieszkan )[1] <- 'zakres_geograficzny'
 names(przewidywana_dlugosc_zycia )[length(przewidywana_dlugosc_zycia)]<- 'przewidywana_dlugosc_zycia'
 names(edukajca_po_populacji )[length(edukajca_po_populacji)]<- 'edukajca'
 names(konsumpcja_owoce_warzywa )[length(konsumpcja_owoce_warzywa)]<- 'konsumpcja_owoce_warzywa'
@@ -100,14 +109,20 @@ ramka_rok_do_roku_zmienne <- ramka_rok_do_roku_zmienne %>%
             cena_pradu = mean(cena_pradu, na.rm = FALSE))
 
 ramka_rok_do_roku_zmienne <- ramka_rok_do_roku_zmienne %>%
-  merge(  wynajem, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
   merge(wartosc_podatku, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
   merge(inflacja, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
   merge(zarobki_kob_euro, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
   merge(zarobki_mez_euro, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
-  merge(Parytety_sily_nabywczej, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+  merge(parytety_sily_nabywczej, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
   merge(bilans_zuzycia_wody, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
-  na.omit()
+  merge(ceny_nowych_mieszkan, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+  merge(ceny_uzywanych_mieszkan, by = c('zakres_geograficzny','okres_czasu'), all= TRUE) %>%
+  mutate(sr_zarobki = round(średnie_roczne_zarobki_kobiet+średnie_roczne_zarobki_mężczyzn)/2) %>%
+na.omit()
+
+
+
+
 
 #łączenie 2 ramki danych
 konsumpcja_owoce_warzywa<-konsumpcja_owoce_warzywa %>%
